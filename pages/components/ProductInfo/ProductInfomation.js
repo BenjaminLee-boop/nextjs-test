939920692f84fc18a7419daba38ac092caa3039e
Benjamin Lee <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-09-19 09:43:50 +0930

    Feat: Product info card
    Added product info

---
+++ pages/components/ProductInfo/ProductInfomation.js
@@ -0,0 +1,27 @@
+import OptionsSelector from "./OptionsSelector";
+
+function ProductInfomation({ data }) {
+  console.log(data);
+  return (
+    <div className="pl-5 pt-5">
+      <div>
+        <h1 className="text-2xl font-sans">{data.title}</h1>
+        <p>
+          {`$${data.priceRange.maxVariantPrice.amount} ${data.priceRange.maxVariantPrice.currencyCode}`}
+        </p>
+        <p className="mt-4 text-l">{data.description}</p>
+      </div>
+      {data.options.map((option, index) => (
+        <OptionsSelector options={option} key={index}></OptionsSelector>
+      ))}
+
+      <div className="mt-5">
+        <button className=" px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductInfomation;
