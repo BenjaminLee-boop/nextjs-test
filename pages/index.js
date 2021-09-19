9de85ff8f33ea7c300fab8b609184c359b62d867
Benjamin Lee <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-09-19 09:42:44 +0930

    feat: added the main Product info Page to be rendered
    Added the main product info component to be rendered in index.js.

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,33 @@
 import Head from "next/head";
 import { getProductBySlug } from "../lib/api";
 import { Client } from "../pages/_app";
+import ProductInfo from "./components/ProductInfo";
 
 export async function getStaticProps() {
-	const { data } = await Client.query({
-		query: getProductBySlug,
-	});
-	return {
-		props: {
-			data: data,
-		},
-	};
+  const { data } = await Client.query({
+    query: getProductBySlug,
+  });
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
 
 export default function Home({ data }) {
-	const product = data.productByHandle;
-
-	console.log(product);
-	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<Head>
-				<title>{product.title}</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-				render a product page here.
-			</main>
-
-			<footer className="flex items-center justify-center w-full h-24 border-t">
-				footer
-			</footer>
-		</div>
-	);
+  const product = data.productByHandle;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>{product.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col  w-full flex-1 px-20">
+        <ProductInfo data={product}></ProductInfo>
+      </main>
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        footer
+      </footer>
+    </div>
+  );
 }
